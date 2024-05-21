914a3b03dd31332c77b706427482c16dffdad9ab
valento <valentin.mundrov@gmail.com>
2024-05-21 10:45:24 +0300

    init, basic styling

--- views/jsx/Components/Home.jsx
+++ views/jsx/Components/Home.jsx
@@ -1,19 +1,27 @@
 /** You can submit this test using either Handlebars or JSX as a templating engine. This is the file to work in if you would like to use JSX */
 
-import React from 'react';
+import { useEffect } from "react"
+import React, { useEffect, useState } from 'react'
 
-export default function Home(props) {
-
+export default function Home({ pageTitle, data }) {
 	const hero = (
 		<>
-			<h1>{props.pageTitle}</h1>
+			<h1>{pageTitle}</h1>
 		</>
-	);
+	)
 
 	return (
 		<>
 			{hero}
-			<p>{props.content}</p>
+			<div style={{display: 'flex', flexDirection: 'row', flex: 1, gap: 20}}>
+			{
+				data.data.map( (item, ind) => (
+					<p key={ind}>{JSON.parse(JSON.stringify(item.items[0].symbolInput))} :
+						{(Number(JSON.stringify(item.items[0].quote.lastPrice)).toFixed(2)).concat('%')}
+					</p>
+				))
+			}
+			</div>
 		</>
 	);
 };
--- views/jsx/Main.jsx
+++ views/jsx/Main.jsx
@@ -4,12 +4,6 @@ import Home from './Components/Home';
 
 export default function Main(props) {
 
-	if(props.data !== undefined) {
-		console.log((props.data.data[0]))
-	} else {
-		console.log('Main Component: No new data...');
-	}
-
 	return (
 		<html lang="en">
 			<head>
@@ -17,6 +11,9 @@ export default function Main(props) {
 				<meta name="viewport" content="width=device-width, intial-scale=1.0" />
 				<title>Financial Times technical test</title>
 				<link rel="stylesheet" type="text/css" href="/dist/css/styles.css" />
+				<link rel="stylesheet" href="https://www.ft.com/__origami/service/build/v3/bundles/css?components=o-grid@^6.0.0&brand=core&system_code=$$$-no-bizops-system-code-$$$"/>
+				
+				<script src="https://www.ft.com/__origami/service/build/v3/bundles/js?components=o-table@^9.0.2,o-autoinit@^3.0.0&system_code=$$$-no-bizops-system-code-$$$" defer></script>
 				<script src="/dist/js/main.js" defer></script>
 			</head>
 			<body>
